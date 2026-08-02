842691152764f66324dce4af7a047bada4794eaf
Devansh Singh <ds109@snu.edu.in>
2026-08-02 21:23:12 +0530

    Registration

---
+++ registerUser.py
@@ -0,0 +1,78 @@
+import os
+from dotenv import load_dotenv
+import uuid
+import asyncio
+from database import session
+from user_checks import user_exists,check_readme
+
+
+load_dotenv()
+
+length=int(os.getenv("RANDOM_STRING_LENGTH"))
+sleep_time=int(os.getenv("SLEEP_TIME1"))
+message="""Follow these steps to verify your LeetCode username
+           1) Go to your Leetcode profile and click on Edit Profile.
+           2) Change the ReadMe in General to {} within the given time limit of {} seconds.
+        """
+async def register(ctx,username : str):
+    author=str(ctx.author.id)
+
+    query1="""SELECT username 
+              FROM LCDisc.Users
+              WHERE author=%s
+           """
+    check=session.execute(query1,(author,)).one()
+
+    if check:
+        user=check.username
+        await ctx.send(f"User is already verified and is registered with the username {user}")
+        return 
+    else:
+        if await user_exists(username):
+
+            random_string=str(uuid.uuid4()).replace('-','')[:length]
+            await ctx.send(message.format(random_string,sleep_time))
+            await asyncio.sleep(sleep_time)
+            changed_readme=await check_readme(username)
+
+            if changed_readme==random_string:
+
+                query2="""INSERT INTO LCDisc.Users
+                (author,username)
+                VALUES (%s,%s)
+                """
+
+                session.execute(query2,(author,username))
+
+                await ctx.send("The Leetcode username is successfully verified.")
+                return
+            else:
+                await ctx.send("The ReadMe does not match.Please try again.")
+                return
+        else:
+            await ctx.send("Wrong username entered.Please try again.")
+            return
+async def unregister(ctx,):
+    author=str(ctx.author.id)
+
+    query1="""SELECT username
+    FROM LCDisc.Users 
+    WHERE author=%s
+    """
+
+    check=session.execute(query1,(author,)).one()
+
+    if check:
+
+        query2="""DELETE FROM LCDisc.Users
+        WHERE author=%s
+        """
+
+        session.execute(query2,(author,))
+
+
+        await ctx.send("User successfully unregistered")
+        return
+    else:
+        await ctx.send("User is already unregistered")
+        return
